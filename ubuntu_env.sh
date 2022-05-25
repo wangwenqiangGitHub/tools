803cc1a74ccb28bcab6a183a7eeb23103bf81aaf
wangwenqiangGitHub <1103169969@qq.com>
2022-05-25 10:26:28 +0800

    ubuntu vim config

---
+++ ubuntu_env.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+#======================================================================
+#
+# install_vim.sh - 
+#
+# Created by wwq on 2022/05/25
+# Last Modified: 2022/05/25 10:21:15
+#
+#======================================================================
+set -x
+
+sudo apt install gcc g++ make cmake gdb
+sudo apt install clangd
+sudo apt install nodejs
+sudo apt install yarn
+sudo apt install ripgrep
