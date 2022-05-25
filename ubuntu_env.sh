bcad009d936fd8ec61b10810d218652f55c5d6c0
wangwenqiangGitHub <1103169969@qq.com>
2022-05-25 10:28:02 +0800

    gtags install

--- ubuntu_env.sh
+++ ubuntu_env.sh
@@ -14,3 +14,13 @@ sudo apt install clangd
 sudo apt install nodejs
 sudo apt install yarn
 sudo apt install ripgrep
+
+#----------------------------------------------------------------------
+# gtags
+#----------------------------------------------------------------------
+sudo apt-get install libncurses-dev
+wget https://ftp.gnu.org/pub/gnu/global/global-6.6.tar.gz
+cd global-6.6
+./config
+make
+sudo make install
