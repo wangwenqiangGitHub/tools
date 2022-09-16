714b5db073afd0c52b9d178e759f1672bbc5b737
wangwenqiangGitHub <1103169969@qq.com>
2022-09-16 09:39:01 +0800

    [feat][cpp] 打印时间

--- cpp_tool/timeStamp.cpp
+++ cpp_tool/timeStamp.cpp
@@ -40,3 +40,28 @@ std::string Stamp2Time(uint64_t timestamp)
         str = str+ " " + std::to_string(ms);
         return str;
 }
+
+// 打印时间
+std::string printTime()
+{
+	struct timeval _tv;
+	struct tm _tm;
+  gettimeofday(&_tv, NULL);
+  localtime_r(&_tv.tv_sec, &_tm);
+  char buf[128];
+  snprintf(buf, sizeof(buf), "%d-%02d-%02d %02d:%02d:%02d.%03d",
+		  1900 + _tm.tm_year,
+		  1 + _tm.tm_mon,
+		  _tm.tm_mday,
+		  _tm.tm_hour,
+		  _tm.tm_min,
+		  _tm.tm_sec,
+		  (int) (_tv.tv_usec / 1000));
+  return buf;
+}
+
+int main()
+{
+	printf("%s\n", printTime().c_str());
+	return 0;
+}
