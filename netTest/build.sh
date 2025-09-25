7e481e2dc9addc01d8ed502d7a3d9028968d1c4c
wangwenqiangGitHub <1103169969@qq.com>
2025-09-25 13:37:05 +0800

    [feat] clang-dity单个文件检查

--- netTest/build.sh
+++ netTest/build.sh
@@ -13,3 +13,5 @@ ln -s build/compile_commands.json .     # 软链接到项目根目录
 # run-clang-tidy -p build/ -checks='*' -j4  # 使用4个线程
 # run-clang-tidy -p build/ > clang-tidy-report.txt
 run-clang-tidy -p build/
+
+# clang-tidy-14 --use-color -p=~ code/xx.cpp -extra-arg=-isystem -extra-arg=/usr/include/c++/11 -extra-arg=-isystem -extra-arg=/usr/include/x86_64-linux-gnu/c++/11  -header-filter=.\*    -system-headers -checks="-\*,bugprone-shift-too-many"
